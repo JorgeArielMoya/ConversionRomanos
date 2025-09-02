ce0c610230985c009bfa2b2c14d021d169296a35
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-02 00:47:27 -0400

    Pedimos numero romano

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Ingrese el numero romano: ");
+            string? numRomano = Console.ReadLine();  
         }
     }
 }
