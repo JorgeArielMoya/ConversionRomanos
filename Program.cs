e264776d6a1af383ce49f0123e5c85d947795cd0
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-02 01:18:45 -0400

    Imprimimos por consola y validamos que el numero romano ingresado sea uno valido

--- Program.cs
+++ Program.cs
@@ -5,7 +5,17 @@
         static void Main(string[] args)
         {
             Console.Write("Ingrese el numero romano: ");
-            string? numRomano = Console.ReadLine();  
+            string? numRomano = Console.ReadLine();
+
+            int numeroConvertido = ConversionesHelper.ConvertirRomanoDecimal(numRomano);    
+
+            if (numeroConvertido != -1)
+            {
+                Console.WriteLine ($"El numero romano {numeroConvertido} en decimal es {numeroConvertido}");
+                return;
+            }
+
+            Console.WriteLine("Numero romano no valido");
         }
     }
 }
