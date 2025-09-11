9fd98c5de28e1c173fd60d5d32a8c2bf92a4be14
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-10 22:38:30 -0400

    Se creo nueva rama y clase de operacioneshelper

---
+++ OperacionesHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionRomanos
+{
+    internal class OperacionesHelper
+    {
+        public static int CalcularFactorial (int num)
+        {
+            for (int i = num; i > 0; i++)
+            {
+                num *= i;
+            }
+
+            return num;
+        }
+    }
+}
